e3088a99539745b82cfed38f996a7d5ea583a480
Geoffrey Gilmore <tkdmaster123@gmail.com>
2020-09-16 08:48:55 -0700

    ci: check -> freeze

--- scripts/dhall-freeze.sh
+++ scripts/dhall-freeze.sh
@@ -19,10 +19,10 @@ function my_chronic() {
 
 export -f my_chronic
 
-function check() {
+function freeze() {
   local file="$1"
 
-  local CHECK_ARGS=(
+  local FREEZE_ARGS=(
     "dhall"
     "freeze"
     "--inplace"
@@ -30,10 +30,10 @@ function check() {
   )
 
   if [ "${CHECK:-"false"}" == "true" ]; then
-    FORMAT_ARGS+=("--check")
+    FREEZE_ARGS+=("--check")
   fi
 
-  result=$(my_chronic "${CHECK_ARGS[@]}" 2>&1)
+  result=$(my_chronic "${FREEZE_ARGS[@]}" 2>&1)
   rc=$?
 
   if [ -n "$result" ]; then
@@ -44,6 +44,6 @@ function check() {
 
   return "$rc"
 }
-export -f check
+export -f freeze
 
-./scripts/parallel_run.sh check {} ::: "${DHALL_FILES[@]}"
+./scripts/parallel_run.sh freeze {} ::: "${DHALL_FILES[@]}"
